13f076577fcda1a10e35e55fa6d57755b8a66b42
YPINPIN <yping611@gmail.com>
2025-06-20 21:52:31 +0800

    init project & reset css

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <h1>線上抽籤轉盤｜免費抽獎轉盤工具</h1>
+</template>
+
+<style lang="scss">
+@use "@/styles/reset" as *;
+
+body {
+  font-family: $font-main;
+  background-color: #fff;
+  color: #333;
+  line-height: 1.5;
+}
+</style>
