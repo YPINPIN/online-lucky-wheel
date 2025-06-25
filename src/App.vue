56b6843aa57453362faf45d3328bc259c7efb1a3
YPINPIN <yping611@gmail.com>
2025-06-26 00:37:57 +0800

    add body-bg and header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
-<script setup></script>
+<script setup>
+import AppHeader from "@/components/AppHeader.vue";
+</script>
 
 <template>
-  <h1>線上抽籤轉盤｜免費抽獎轉盤工具</h1>
+  <div class="app-container">
+    <AppHeader />
+  </div>
 </template>
 
 <style lang="scss">
 @use "@/styles/reset" as *;
 
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
 body {
+  min-height: 100vh;
+  background: $body-bg;
   font-family: $font-main;
-  background-color: #fff;
-  color: #333;
+  color: $font-color;
   line-height: 1.5;
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="app-header">
+    <h1 class="app-title">線上抽籤轉盤</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.app {
+  &-header {
+    padding: 16px 0;
+    margin-bottom: 20px;
+    background-color: $base-background-color;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.04);
+    border-bottom: 1px solid $base-border-color;
+    text-align: center;
+  }
+  &-title {
+    font-size: 28px;
+    font-weight: bold;
+    line-height: 1;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.1);
+  }
+}
+</style>
